d09a91d6bcebbd2a8f8c9fc1bbf0b29a31779841
Darina <darinakrasilnikova1@gmail.com>
2025-08-13 21:37:00 +0300

    Реализовала рендеринг тудушек (1 часть задания)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 export default function App() {
-  return;
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/todos")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTodoList(todos.todos);
+      });
+  }, []);
+
+  return (
+    <div className={styles.listTodos}>
+      {todoList.map((todo) => (
+        <div className={styles.message} key={todo.id}>
+          {todo.todo}
+        </div>
+      ))}
+    </div>
+  );
 }
